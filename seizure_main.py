bbb63b277e92a724f17a0d6a909dd5c03aadab48
sadikaVER <50405671+sadikaVER@users.noreply.github.com>
2022-03-21 20:06:04 +0530

    Update seizure_main.py

--- seizure_main.py
+++ seizure_main.py
@@ -1,11 +1,19 @@
 import os
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+import streamlit as st
+import streamlit.components.v1 as stc
+
+
+
 def main():
-        current_path=os.getcwd()
-	st.write(current_path)
-        
+    current_path=os.getcwd()
+    st.write(current_path)
+    
 if __name__ == '__main__':
 	main()
 
+    
 
 
 
